5cc8302b9cf14a256c352275660c9b7990f08c48
Commando98 <mukularora649@gmail.com>
2017-06-22 17:39:09 +0530

    firstapp

--- routes/users.js
+++ routes/users.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  res.send('respond with an rresource');
 });
 router.get('/user', function(req, res, next) {
     res.send('Hello user');
