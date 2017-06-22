b99b0e277104b0268230e28d2da670725e8ad406
Commando98 <mukularora649@gmail.com>
2017-06-22 17:17:21 +0530

    sdsfg

---
+++ routes/index.js
@@ -0,0 +1,16 @@
+var express = require('express');
+var router = express.Router();
+var URL="mongodb://rishvik:Mukul9870@ds131492.mlab.com:31492/commando"
+const db=require('monk')(URL)
+const docs=db.get('collecu')
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+    docs.insert({"name":"Mukul_Arora"}, function (err, docs) {
+        if (err) console.log(err)
+        else res.json(docs[0]);
+    })
+});
+
+
+module.exports = router;
