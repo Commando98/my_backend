732a990b40d8fb017735ab1c91fce259667327d7
Commando98 <mukularora649@gmail.com>
2017-06-26 16:34:58 +0530

    dsfgjs

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ var users = require('./routes/users');
 
 var app = express();
 var cors=require('cors');
-var methodOverride=require(methodoverride)
+var methodOverride=require('method-override')
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -29,6 +29,16 @@ app.use('/users', users);
 app.use(cors());
 app.use(methodOverride());
 
+
+
+app.use(function (req,res,next) {
+    res.header("Access-Control-Allow-Origin","*");
+    res.header("Access-Control-Allow-Methods","DELETE,PUT");
+    res.header("Access-Control-Allow-Headers","Origin,X-Requested-With, Content-Type,Accept");
+    next();
+
+})
+
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   var err = new Error('Not Found');
@@ -36,13 +46,7 @@ app.use(function(req, res, next) {
   next(err);
 });
 
-app.use(function (req,res,next) {
-  res.header("Access-Control-Allow-Origin","*");
-    res.header("Access-Control-Allow-Methods","DELETE,PUT");
-    res.header("Access-Control-Allow-Headers","Origin,X-Requested-With, Content-Type,Accept");
-    next();
 
-})
 
 // error handler
 app.use(function(err, req, res, next) {
