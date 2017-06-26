68913bc4ee7fd2704b74787b7d618b51a0ade5b4
Commando98 <mukularora649@gmail.com>
2017-06-26 16:05:12 +0530

    efjwegj

--- routes/index.js
+++ routes/index.js
@@ -12,7 +12,7 @@ router.get('/', function(req, res, next) {
     })
 });
 router.get('/find',function(req, res, next){
-    docs.find({},function(err, docs){
+    docs.find({"id":"we4567"},function(err, docs){
         if (err) console.log(err)
         else res.json(docs[0]);
     })
