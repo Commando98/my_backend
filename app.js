e09bbcc15834890a3f5a15b06f7c73ddfe18dfb4
Commando98 <mukularora649@gmail.com>
2017-06-26 16:27:34 +0530

    sdf

--- app.js
+++ app.js
@@ -9,6 +9,8 @@ var index = require('./routes/index');
 var users = require('./routes/users');
 
 var app = express();
+var cors=require('cors');
+var methodOverride=require(methodoverride)
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -24,6 +26,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
 app.use('/users', users);
+app.use(cors());
+app.use(methodOverride());
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -31,6 +35,14 @@ app.use(function(req, res, next) {
   err.status = 404;
   next(err);
 });
+
+app.use(function (req,res,next) {
+  res.header("Access-Control-Allow-Origin","*");
+    res.header("Access-Control-Allow-Methods","DELETE,PUT");
+    res.header("Access-Control-Allow-Headers","Origin,X-Requested-With, Content-Type,Accept");
+    next();
+
+})
 
 // error handler
 app.use(function(err, req, res, next) {
