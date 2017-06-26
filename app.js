e54a1e2583a1199ddbcd064e81cac1becece116f
Commando98 <mukularora649@gmail.com>
2017-06-26 16:39:40 +0530

    dsbfjsdgf

--- app.js
+++ app.js
@@ -33,7 +33,7 @@ app.use(methodOverride());
 
 app.use(function (req,res,next) {
     res.header("Access-Control-Allow-Origin","*");
-    res.header("Access-Control-Allow-Methods","DELETE,PUT");
+    res.header("Access-Control-Allow-Methods",'DELETE,PUT');
     res.header("Access-Control-Allow-Headers","Origin,X-Requested-With, Content-Type,Accept");
     next();
 
